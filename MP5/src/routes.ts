620bff7b52c731ae13d75a631f4c05b9ea763643
Kamil <sikorakamil26@gmail.com>
2021-06-04 19:53:58 +0200

    add user works

--- MP5/src/routes.ts
+++ MP5/src/routes.ts
@@ -1,6 +1,7 @@
 import { Express, Request, Response } from "express";
 import { createUserHandler } from "./contoller/user.controller";
 import validateRequest from "./middleware/validateRequest";
+import { createUserSchema } from "./schema/user.schema";
 
 export default function (app: Express) {
   app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
