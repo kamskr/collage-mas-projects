ef2b534a14f638dda5fec4c23b6e3e2be4d1f67c
Kamil <sikorakamil26@gmail.com>
2021-07-07 22:27:27 +0200

    fixed cors error

--- Z-kom/src/server.ts
+++ Z-kom/src/server.ts
@@ -4,6 +4,7 @@ import { deserializeUser } from "./middleware";
 import connect from "./db/connect";
 import routes from "./routes";
 import log from "./logger";
+var cors = require("cors");
 
 export const setUpServer = () => {
   const port = config.port as number;
@@ -12,6 +13,7 @@ export const setUpServer = () => {
   const app = express();
   app.use(deserializeUser);
   // Parses incoming requests with JSON payloads
+  app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
 
